651ca69d3ebc03a94e3af5d586e85306eeb3eb40
Greice Moreira <greice.f.d.a@hotmail.com>
2025-05-12 20:36:05 -0400

    base

---
+++ project1-api/backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const port = 8080;
+
+app.use('/', require("./routes"))
+app.listen(process.env.port || port);
+
+console.log('Web Server ir listening at port' + (process.env.port || port));
