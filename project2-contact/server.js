cee57ccdc1b12530f15b9764909c4d922aec79e7
Greice Moreira <greice.f.d.a@hotmail.com>
2025-05-19 16:23:43 -0400

    swagger

--- project2-contact/server.js
+++ project2-contact/server.js
@@ -5,7 +5,12 @@ const dotenv = require('dotenv').config();
 const app = express();
 const port = (process.env.PORT || 8080);
 
-app.use(express.json());
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json')
+
+app
+    .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+    .use(express.json())
 
 mongoose.connect(process.env.MONGODB_URL)
     .then( () => {
