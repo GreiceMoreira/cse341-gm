efa6a5647faa0ae5d2a5573cf3da34150a33bb28
Greice Moreira <greice.f.d.a@hotmail.com>
2025-05-21 14:48:11 -0400

    swagger

--- project3-wellness/server.js
+++ project3-wellness/server.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
 
 const entryRoutes = require('./routes/entries');
+const swaggerRoute = require('./routes/swagger')
 
 const app = express();
 const port = (process.env.PORT || 8080);
 
 app
     .use(express.json())
+    .use(swaggerRoute)
     .use('/api/entries', entryRoutes);
 
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URI)
     .then( () => {
     console.log('MongoDB connected!');
     app.listen(port, () => {
-        console.log('Web Server ir listening at port ', port)
+        console.log('Web Server is listening at port ', port)
     }
     )})
 
---
+++ project3-wellness/swagger.js
@@ -0,0 +1,47 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: "Mood Journal API",
+        description: "API to create and track mood journal entries including sleep, hydration, and exercise.",
+        contact: {
+            name: "Greice Moreira",
+            email: "deandradegreice@gmail.com",
+          },
+          version: "1.0.0"
+    },
+    host: "cse341-gm.onrender.com",
+    schemes: ["https"],
+
+    definitions: {
+        User: {
+        id: "123456789",
+        email: "user@example.com",
+        password: "strongpassword123",
+        name: "Jane Doe",
+        age: 30,
+        avatar: 1
+        },
+        Entry: {
+            user: "userId123",
+            date: "2025-05-20",
+            mood: 4,
+            exercise: true,
+            water: 8,
+            sleepHours: 7.5,
+            bestMemory: "Had lunch with my friend",
+            gratitude: "My health"
+        }
+    },
+
+    externalDocs: {
+        description: "GitHub Repository",
+        url: "https://github.com/GreiceMoreira/cse341-gm/tree/main/project3-wellness"
+      }
+
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./server.js']; 
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
